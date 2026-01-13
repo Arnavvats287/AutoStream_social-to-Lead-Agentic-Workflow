78a7bba031ad11c492dc693084627ef2349776b6
Arnavvats287 <151434574+Arnavvats287@users.noreply.github.com>
2026-01-13 23:32:00 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
-
-llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0,google_api_key="AIzaSyCHNAndwaci1TdWoM1aCYPi3vKh49koX6c"
+from dotenv import load_dotenv
+load_dotenv()
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0
 )
 
 response = llm.invoke("Say hello")
